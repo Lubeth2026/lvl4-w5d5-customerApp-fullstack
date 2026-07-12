732fa98b14dd42bafac5d15988ca5ca7d69f5b4d
Lubeth <lubethjimenez@yahoo.com>
2026-07-12 15:14:17 -0400

    Add CSS styling to the NavBar in React & CSS styling to Home.jsx page in React

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,10 +4,10 @@ import React from 'react'
 function Home() {
 
   return (
-    <div>
-      <h1>Welcome!</h1>
-      <p>Welcome to the Customer Management Application.</p>
-      <p>Use the navigation bar to create a customer or view customer history.</p>
+    <div className="home-container">
+      <h1 className="home-heading">Welcome!</h1>
+      <p className="home-paragraph">Welcome to the Customer Management Application.</p>
+      <p className="home-paragraph">Use the navigation bar to create a customer or view customer history.</p>
     </div>
   );
 }
