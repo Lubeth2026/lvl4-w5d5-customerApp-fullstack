377b465353d13935a359d10b8b0536f5d53bd646
Lubeth <lubethjimenez@yahoo.com>
2026-07-12 15:41:57 -0400

    Add CustomerList.jsx state & getCustomers function to map over the customers data in Flask which displays in React page

--- frontend/src/components/CustomerList.jsx
+++ frontend/src/components/CustomerList.jsx
@@ -1,10 +1,33 @@
 
-import React from 'react'
+import React, { useDebugValue, useEffect, useState } from 'react'
 
 function CustomerList() {
+
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(()=>{
+    async function getCustomers() {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/api/customers");
+        const data = await response.json();
+        setCustomers(data.customers)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getCustomers();
+  }, [])
+
   return (
     <div>
         <h1>Customer History</h1>
+        {customers.map((customer)=> (
+          <div key={customer.id}>
+            <h2>{customer.first_name} {customer.last_name}</h2>
+            <p>{customer.email}</p>
+            <p>{customer.city} {customer.state}</p>
+          </div>
+        ))}
     </div>
   )
 }
