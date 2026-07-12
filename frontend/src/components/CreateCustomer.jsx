06d8aeda83a6ad2e6101ca5da2663dc29aa47ca3
Lubeth <lubethjimenez@yahoo.com>
2026-07-12 17:09:13 -0400

    Add CSS styling to CreateCustomer.jsx form in React page

--- frontend/src/components/CreateCustomer.jsx
+++ frontend/src/components/CreateCustomer.jsx
@@ -20,36 +20,37 @@ function CreateCustomer() {
             console.log(error)
         }
     }
+
   return (
-    <div>
+    <div className="create-container">
         <h1>Create Customer</h1>
-        <form onSubmit={handleSubmit}>
-            <div>
+        <form onSubmit={handleSubmit} className="customer-form" >
+            <div className="form-group" >
                 <label htmlFor="first_name">First Name</label>
                 <input type="text" value={formData.first_name} 
                 onChange={(event)=> setFormData({...formData, first_name: event.target.value})} />
             </div>
-            <div>
+            <div className="form-group" >
                 <label htmlFor="last_name">Last Name</label>
                 <input type="text" value={formData.last_name} 
                 onChange={(event)=> setFormData({...formData, last_name: event.target.value})} />
             </div>
-            <div>
+            <div className="form-group" >
                 <label htmlFor="email">Email</label>
                 <input type="text" value={formData.email} 
                 onChange={(event)=> setFormData({...formData, email: event.target.value})} />
             </div>
-            <div>
+            <div className="form-group" >
                 <label htmlFor="city">City</label>
                 <input type="text" value={formData.city} 
                 onChange={(event)=> setFormData({...formData, city: event.target.value})} />
             </div>
-            <div>
+            <div className="form-group" >
                 <label htmlFor="state">State</label>
                 <input type="text" value={formData.state} 
                 onChange={(event)=> setFormData({...formData, state: event.target.value})} />
             </div>
-            <button type="submit">Create Customer</button>
+            <button type="submit" className="create-button" >Create Customer</button>
         </form>
     </div>
   )
