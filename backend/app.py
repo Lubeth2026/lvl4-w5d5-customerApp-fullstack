5358daa1287fdf367871704cef6d0f5b6aaaa22f
Lubeth <lubethjimenez@yahoo.com>
2026-07-12 16:44:59 -0400

    Add POST create_customer route to accept the CREATE requests from React formData & create new customer through Flask in supabase table customers

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 from supabase import create_client, Client
@@ -18,8 +18,23 @@ supabase: Client = create_client(
 def health():
     return {"Status": "Good"}
 
-@app.get("/customers")
+@app.get("/api/customers")
 def customers_list():
     response = supabase.table("customers").select("*").execute()
     return {"customers": response.data}
 
+@app.post("/api/customers")
+def create_customer():
+    data = request.get_json()
+
+    response = supabase.table("customers").insert({
+        "first_name": data["first_name"],
+        "last_name": data["last_name"],
+        "email": data["email"],
+        "city": data["city"],
+        "state": data["state"]
+    }).execute()
+    return {"message": "Customer was Created", "customer": response.data}
+
+if __name__ == "__main__":
+    app.run(debug=True)
